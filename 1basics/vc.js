227efdd00411f1276137350fccb613f9c4873f1a
Laxus925 <ashimkhatri25@gmail.com>
2024-11-09 02:50:25 +0000

    variables and  constants

---
+++ 1basics/vc.js
@@ -0,0 +1,26 @@
+const rollno=23;
+//rollno=5
+console.log(rollno);
+
+var name="ashim";
+name="khatri";
+console.log(name);
+//lets not use var cuz of issue in funcation and scope
+
+let email="ashim@1";
+email="changed@1";
+console.log(email);
+
+/*phone=nep56
+console.log(phone);*/
+
+let city;
+
+console.table([rollno,name,email,city])
+
+
+
+
+
+
+
