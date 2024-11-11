79f1eccf46b0d340413b16a79cdcd12b16b0b819
Laxus925 <ashimkhatri25@gmail.com>
2024-11-11 02:42:28 +0000

    datatypes

---
+++ 1basics/datatypes.js
@@ -0,0 +1,18 @@
+//"use strict"; //treats all js code as newer version
+//alert("bawdba") we areusing nodejs not browser
+//also make good code redeability
+//datatyes
+/*Number
+BigInt
+String
+boolean=true or false
+null
+undefined
+symbol*/
+
+let available= nullfv
+console.log(available)
+console.log(typeof "false");
+console.log(typeof available);
+
+
