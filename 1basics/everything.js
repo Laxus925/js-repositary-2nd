34cd3c49e1c270706dfb38d654aa777224ac1801
Laxus925 <ashimkhatri25@gmail.com>
2024-11-15 02:44:43 +0000

    everythig

---
+++ 1basics/everything.js
@@ -0,0 +1,13 @@
+let value=2;
+let megvalue=-value;
+// console.log(megvalue);
+// console.log(2-3);
+// console.log(2**3);
+let str1="ashim "
+let str2="khatri"
+console.log(str1+str2);
+
+console.log("1");
+
+
+
