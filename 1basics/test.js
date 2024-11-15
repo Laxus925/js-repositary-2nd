a3b9f66bd9d31b7779588c5e49efa7c1d1503f05
Laxus925 <ashimkhatri25@gmail.com>
2024-11-15 02:08:36 +0000

    data type cponversion

--- 1basics/test.js
+++ 1basics/test.js
@@ -1,1 +1,27 @@
-console.log("1hello world");
+console.log("data type convrsion");
+let score="64hl";
+console.log(typeof score);;
+console.log(typeof (score));
+let valueInNumber=Number(score);
+console.log(typeof (valueInNumber));
+let string="ipfew"
+let changedatatype=Number(string);
+console.log(typeof changedatatype);
+console.log(changedatatype);
+
+/* "33" canbe changed into nunmber
+"33abc" cannot be
+bollean values like TRUE change into 1 and FALSE into 2
+33 into string . value will come out as 33*/
+
+let boolean=0;
+let b=Boolean(boolean);
+console.log(typeof b);;
+console.log(b);
+
+//1 into TRUE . 0 into FALSE. "" null into false. "strings" into true
+
+
+
+
+
