87c059a32f2bf50e83db6440597d7c2635d3967f
Laxus925 <ashimkhatri25@gmail.com>
2024-11-17 16:00:02 +0000

    datatype comparision

--- 1basics/everything.js
+++ 1basics/everything.js
@@ -1,13 +1,40 @@
-let value=2;
-let megvalue=-value;
-// console.log(megvalue);
-// console.log(2-3);
-// console.log(2**3);
-let str1="ashim "
-let str2="khatri"
-console.log(str1+str2);
+// // let value=2;
+// // let megvalue=-value;
+// // // console.log(megvalue);
+// // // console.log(2-3);
+// // // console.log(2**3);
+// // let str1="ashim "
+// // let str2="khatri"
+// // console.log(str1+str2);
 
-console.log("1");
+// // console.log("1"+2);
+// // console.log(1+2+"3");
+// // console.log(1+"2"+3);
+// // console.log(+true);
+
+// // let num1,num2,num3;
+// // num1=num2=num3=2+3;
+// // console.log(num1);
+
+// // let lives=100;
+// // lives++;
+// // ++lives;
+// // console.log(lives);
+
+// //comparision operators >     >=     <      <=      ==       !=  comparision opeator trats null as a 0 number
+// console.log(2>3);
+// console.log(null<0)
+// console.log(null>=0)
+// console.log(null<=0);
+
+console.log(undefined>=0)
+these above should be avoided
+
+//console.log("2"=0);
+console.log("2"===0);
+
+
+
 
 
 
